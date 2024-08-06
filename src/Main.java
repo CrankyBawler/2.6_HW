55fea8c5d5dbe36b0284839a083877790fd77c24
Cranky Bawler <6666lenia6666@mail.ru>
2024-08-06 17:05:28 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,58 @@
+import java.util.*;
+
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();
+        task2();
+        task3();
+        task4();
     }
+
+    public static void task1() {
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+
+            }
+
+
+        }
+        System.out.println();
+
+
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println(num);
+                prevNum = num;
+            }
+        }
+    }
+
+    public static void task3() {
+        Set<String> uniqueStrings = new HashSet<>(strings);
+        System.out.println(uniqueStrings);
+    }
+
+    public static void task4() {
+        Map<String, Integer> strings1 = new HashMap<>();
+        for (String string : strings) {
+            if (!strings1.containsKey(string)) {
+                strings1.put(string, 1);
+            } else {
+                strings1.put(string, strings1.get(string) + 1);
+            }
+        }
+        System.out.println(strings1.values());
+    }
+
 
 }
